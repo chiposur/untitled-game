0a636240e9bb2cccc1a60b021d5c590892962d7f
chiposur <osurov314@gmail.com>
2022-01-25 21:00:07 -0800

    Added primitive procedural generation of stars zooming by

--- game.js
+++ game.js
@@ -1,20 +1,23 @@
 const state = {
     screen: 'logo',
     logoTimeLeftMs: 3000,
+    stars: [],
+    starGenCooldownMs: 0,
+    starVel: { x: 0, y: 1 },
 };
 const updateIntervalMs = 20;
-const logoBg = '#000000';
 const logoFg = '#AB1C50';
 const logoTxtStyle = '48px arial';
 const logoTxt = 'chiposur';
-const titleBg = '#000000';
 const titleFg = '#4fbcf7';
 const titleTxtStyle = '100px arial';
 const titleTxt = 'Untitled'
-const startTxtStyle = '24px arial';
-const startTxt = 'Start';
-const startTxtColor = '#FFFFFF';
-const startBg = 'gray';
+const startBtnTxtStyle = '24px arial';
+const startBtnTxt = 'Start';
+const startBtnTxtColor = 'white';
+const startBtnBg = 'gray';
+const starColor = 'white';
+const starRadius = 1;
 
 let updateLoop = null;
 let canvas = null;
@@ -27,27 +30,69 @@ function start(id) {
 }
 
 function update() {
+    handleStarGen();
     ctx.save();
     ctx.clearRect(0, 0, 640, 480);
     if (state.screen === 'logo') {
         if (state.logoTimeLeftMs > 0) {
-            ctx.save();
-            ctx.fillStyle = logoBg;
-            ctx.fillRect(0, 0, canvas.width, canvas.height);
-            ctx.restore();
+            drawBlackBg();
             drawLogoFg();
             state.logoTimeLeftMs -= updateIntervalMs;
         } else {
             state.screen = 'title'
         }
     } else if (state.screen === 'title') {
-        ctx.save();
-        ctx.fillStyle = titleBg;
-        ctx.fillRect(0, 0, canvas.width, canvas.height);
-        ctx.restore();
+        drawSpaceBg();
         drawTitleFg();
+    } else if (state.screen = 'game') {
+        drawSpaceBg();
+        drawSprites();
     }
     ctx.restore();
+}
+
+function handleStarGen() {
+    updateStarCoords();
+    if (state.starGenCooldownMs === 0) {
+        genStarRow();
+        state.starGenCooldownMs = 400;
+    }
+    state.starGenCooldownMs -= updateIntervalMs;
+}
+
+function updateStarCoords() {
+    const stars = [];
+    for (let i = 0; i < state.stars.length; ++i) {
+        const star = state.stars[i];
+        star.y += state.starVel.y;
+        if (star.y <= 480) {
+            stars.push(star);
+        }
+    }
+    state.stars = stars;
+}
+
+function genStarRow() {
+    // Generate row of stars
+    // - Same x diff y
+    // - Use randomization to space them out
+    let x = -32;
+    while (x <= 640) {
+        x += Math.floor(Math.random() * 12 + starRadius * 32);
+        state.stars.push({ x: x, y: 0 });
+    }
+}
+
+function drawBlackBg() {
+    ctx.save();
+    ctx.fillStyle = 'black';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    ctx.restore();
+}
+
+function drawSpaceBg() {
+    drawBlackBg();
+    drawStars();
 }
 
 function drawLogoFg() {
@@ -125,20 +170,40 @@ function drawTitleFg() {
 
         // Start btn
         ctx.save();
-        ctx.fillStyle = startBg;
-        ctx.font = startTxtStyle;
-        textMetrics = ctx.measureText(startTxt);
+        ctx.fillStyle = startBtnBg;
+        ctx.font = startBtnTxtStyle;
+        textMetrics = ctx.measureText(startBtnTxt);
         padding = 8;
         x = canvas.width / 2 - textMetrics.width / 2 - padding;
         y = canvas.height / 2 + 40 - 24;
         ctx.fillRect(x, y, textMetrics.width + padding * 2, 24 + padding);
         ctx.restore();
         ctx.save();
-        ctx.fillStyle = startTxtColor;
-        ctx.font = startTxtStyle;
+        ctx.fillStyle = startBtnTxtColor;
+        ctx.font = startBtnTxtStyle;
         ctx.textAlign = 'center';
         x = canvas.width / 2;
         y = canvas.height / 2 + 40;
-        ctx.fillText(startTxt, x, y);
+        ctx.fillText(startBtnTxt, x, y);
         ctx.restore();
+}
+
+function drawStars() {
+    for (let i = 0; i < state.stars.length; ++i) {
+        const star = state.stars[i];
+        drawStar(star.x, star.y);
+    }
+}
+
+function drawStar(x, y) {
+    ctx.save();
+    ctx.fillStyle = starColor;
+    ctx.beginPath();
+    ctx.arc(x, y, starRadius, 0, 2 * Math.PI);
+    ctx.fill();
+    ctx.restore();
+}
+
+function drawSprites() {
+    // Draw assets including spaceship, meteors, etc.
 }
